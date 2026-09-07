0d3e6d4d37459c6f3cda0d7cb2b4618ad559351d
kareemmmm <kareemmattr1994@gmail.com>
2026-09-07 07:54:50 +0300

    make file_storage.py excutable

---
+++ models/engine/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Initializes the package and creates a unique FileStorage instance."""
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
