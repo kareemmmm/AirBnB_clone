774a6e1f1ef2257595796f9ee6d8884611d981b8
kareemmmm <kareemmattr1994@gmail.com>
2026-09-07 04:56:33 +0300

    implenemting user scema

--- models/user.py
+++ models/user.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+"""Defines the User class."""
+from models.base_model import BaseModel
 
+
+class User(BaseModel):
+    """Represent a User in the AirBnB system."""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
